34183f81e9a379b73400af4b3c8a8a0aa5014dad
Ivens Pontes <ivenscardoso@hotmail.com>
2021-10-04 19:41:47 -0300

    fix active links

--- resources/views/layouts/adminlte/layout.blade.php
+++ resources/views/layouts/adminlte/layout.blade.php
@@ -62,5 +62,20 @@
 <script src="{{ asset('assets/adminlte/dist/js/adminlte.min.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 {{-- <script src="assets/adminlte/dist/js/demo.js"></script> --}}
+
+<script>
+    /** add active class and stay opened when selected */
+    var url = window.location;
+
+    // for sidebar menu entirely but not cover treeview
+    $('ul.nav-sidebar a').filter(function() {
+        return this.href == url;
+    }).addClass('active');
+
+    // for treeview
+    $('ul.nav-treeview a').filter(function() {
+        return this.href == url;
+    }).parentsUntil(".nav-sidebar > .nav-treeview").addClass('menu-open').prev('a').addClass('active');
+</script>
 </body>
 </html>
